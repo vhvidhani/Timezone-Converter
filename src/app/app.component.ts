87f1845700daaf0d3c3dc3253bc8211df8c0b92a
vishakha <vhvidhani12@gmail.com>
2020-05-30 13:54:45 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'time-converter';
+  clock= '';
+  //@ViewChild('clockValue' , {static:true}) clockValue: ElementRef;
+
+  constructor()  {}
+
+ 
+
+  updateTime() {
+    const now=moment();
+    const humanReadable= now.format('h:mm:ss A');
+    clock=humanReadable;
+  }
+
+  ngOnInit() {
+    setInterval(updateTime,1000);
+    updateTime();
+  }
+  
+
 }
